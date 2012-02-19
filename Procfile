cfea93790b7c971744b82c8854ddaf9e71fd490c
gnandiga <gnandiga@gmail.com>
2012-02-19 16:25:48 +0000

    "Adding to heroku"

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node step0.js
---
+++ step0.js
@@ -0,0 +1,53 @@
+// Serve files relative to the current working directory
+var root = process.cwd();
+// Listen on the alt-http port
+var port = process.env.PORT || 8080;
+
+// Stack up a server and start listening
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+ 
+http.createServer(function (request, response) {
+ 
+    console.log('request starting...');
+     
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.htm';
+         
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
+}).listen(port);
+ 
+// Give the user a nice message on the standard output
+console.log("Serving %s at http://localhost:%s/", root, port);
